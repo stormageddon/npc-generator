6466dd24fd2a564bb18833c8a60c1dfcd298630e
Mike Caputo <mike@caputo.io>
2018-06-29 10:00:27 -0500

    Add additional user input and defaults

--- src/main/scala/npc-builder-test.scala
+++ src/main/scala/npc-builder-test.scala
@@ -2,20 +2,20 @@ package com.test.NpcBuilder
 
 import scala.util.{Random}
 
-class NPC(index: Int) {
-//  def nameChoices: Array[String] = Array("Mike", 
-//    "Argyn",
-//    "Ulrich",
-//    "Viktor",
-//    "Dendrath",
-//    "Clarence",
-//    "Molloc",
-//    "Gunmaar",
-//    "Reindrath",
-//    "Ogden",
-//    "Jared")
+class NPC(val npcName: String = "", val npcClass: String = "", val npcTitle: String = "") {
+  def nameChoices: Array[String] = Array("Mal", 
+    "Argyn",
+    "Ulrich",
+    "Viktor",
+    "Dendrath",
+    "Clarence",
+    "Molloc",
+    "Gunmaar",
+    "Reindrath",
+    "Ogden",
+    "Tess",
+    "Visca")
   
-  def nameChoices: Array[String] = Array("Jared", "Ethan", "Mike", "Dave")
   def titleChoices: Array[String] = Array("the Enormous", 
     "Bone-breaker", 
     "the Chivalrous", 
@@ -41,23 +41,50 @@ class NPC(index: Int) {
   def r = Random
 
   //def name = nameChoices(r.nextInt(nameChoices.length : Int))
-  def name = nameChoices(index)
-  def title = titleChoices(r.nextInt(titleChoices.length : Int))
-  def characterClass = classChoices(r.nextInt(classChoices.length : Int))
+  var name = ""
+  if (npcName != null && npcName != "") {
+    name = npcName
+  }
+  else {
+    name = nameChoices(r.nextInt(nameChoices.length : Int))
+  }
+  //def name = nameChoices(index)
+
+  var title = ""
+  if (npcTitle != null && npcTitle != "") {
+    title = npcTitle
+  }
+  else {
+    title = titleChoices(r.nextInt(titleChoices.length : Int))
+  }
+  
+  var characterClass = ""
+  if (npcClass != null && npcClass != "") {
+    characterClass = npcClass
+  }
+  else {
+    characterClass = classChoices(r.nextInt(classChoices.length : Int))
+  }
+  
   def level = r.nextInt(20) + 1
 }
 
 object NpcBuilder {
   def main(args: Array[String]) {
-    while(true) {
-      println("How many NPCs do you want to generate?")
-      val numNPCs = scala.io.StdIn.readInt()
-      println("Generating an npc on the fly!")
+    println("What name should your npc have (leave blank for random)?")
+    val npcName = scala.io.StdIn.readLine()
 
-      for (i <- 0 to numNPCs - 1) {
-        def npc = new NPC(i)
-        println(npc.name + ", " + npc.title + ", a level " + npc.level + " " + npc.characterClass)
-      }
-    }
+    println("What title should your npc have (leave blank for random)?")
+    val npcTitle = scala.io.StdIn.readLine()
+
+    println("What class should your npc have (leave blank for random)?")
+    val npcClass = scala.io.StdIn.readLine()
+
+    println("Generating an npc on the fly!")
+
+    val npc = new NPC(npcName = npcName, npcClass = npcClass, npcTitle = npcTitle)
+    println(npc.name + ", " + npc.title + ", a level " + npc.level + " " + npc.characterClass)
   }
 }
+
+
